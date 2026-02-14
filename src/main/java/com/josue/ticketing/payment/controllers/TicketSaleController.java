44dc43b500fccdf949b61292f42e1c073c831cf5
Josue Joel Garcia Abata <josuegarcab2@hotmail.com>
2026-02-13 23:17:14 -0500

    Add post mapping on ticket sale controller to start the flow of saling seats

--- src/main/java/com/josue/ticketing/payment/controllers/TicketSaleController.java
+++ src/main/java/com/josue/ticketing/payment/controllers/TicketSaleController.java
@@ -1,11 +1,28 @@
 package com.josue.ticketing.payment.controllers;
 
+import com.josue.ticketing.reservation.dtos.TicketCreateRequest;
+import com.josue.ticketing.reservation.service.TicketSaleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/ticket-sale")
+@RequiredArgsConstructor
 public class TicketSaleController {
 
+    private final TicketSaleService ticketSaleService;
+
+    @PostMapping("/")
+    public ResponseEntity<String> saleTicket(@RequestBody TicketCreateRequest req) {
+        try {
+            return ResponseEntity.ok().body(ticketSaleService.reservate(req));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
 }
